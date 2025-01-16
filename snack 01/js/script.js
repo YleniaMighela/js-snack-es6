70e28ee1e3e6aea7b1efa8a04842c1a54a0a24e0
Ylenia <yleniamighela@gmail.com>
2025-01-16 15:58:41 +0100

    add logic snack 01

--- snack 01/js/script.js
+++ snack 01/js/script.js
@@ -1,2 +1,16 @@
 // Snack 1
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+//  creo un array 
+
+
+
+    // all'interno dell'array inserisco gli oggetti che descrivono la bici con le proprietà nome e peso
+
+
+
+
+// confronto tra le bici chi avrà il peso minore
+
+
+// stampo la bici con peso minore
